16a2e0143959b4e3c1d1910fd65dc9789fc79353
rydeenworks <rydeenworks@gmail.com>
2019-11-23 16:23:11 +0900

    change plan entry

--- org/org-global.el
+++ org/org-global.el
@@ -7,7 +7,7 @@
         '(("t" "Todo entry" entry (file "~/Dropbox/org-for-cat/private/todo.org")
            "* TODO %?\n    :LOGBOOK:\n    - Added: %U\n    :END:" :empty-lines 1)
           ("p" "Plan entry" entry (file+datetree "~/Dropbox/org-for-cat/private/plan.org")
-           "* タスク整理\n%?")
+           "- [ ] %?")
           ("j" "Journal entry" entry (file+datetree "~/Dropbox/org-for-cat/private/journal.org")
            "* 週次レビュー\n- good:%?\n- bad:\n- try:\n")
           ("r" "Review entry" entry (file+datetree "~/Dropbox/org-for-cat/private/review.org") (file "~/Dropbox/org-for-cat/private/template-review.org"))
